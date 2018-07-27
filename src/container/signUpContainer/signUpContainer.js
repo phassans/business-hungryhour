dc05c4b7ef12988273cf17e1cc88e22c48089f3d
Hoa.Dang <Hoa.Dangn@coxautoinc.com>
2018-07-27 09:11:15 +0700

    Adjust signup page

--- src/container/signUpContainer/signUpContainer.js
+++ src/container/signUpContainer/signUpContainer.js
@@ -2,63 +2,98 @@ import React, { Component } from 'react';
 import './signUpContainer.css';
 import { Link } from 'react-router-dom';
 import NavBar from '../../components/navs/signUpLoginNav/signUpLoginNav';
+import TextField from "material-ui/TextField";
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import { BASE_URL } from "../../data/api";
 
 class SignUpContainer extends Component {
   constructor(props) {
     super(props);
-    
-    this.userInfo = {};
-    this.userInfo.email = React.createRef();
-    this.userInfo.name = React.createRef();
-    this.userInfo.password = React.createRef();
+
+    this.state = {
+      name: "",
+      nameError: "",
+      email: "",
+      emailError: "",
+      password: "",
+      passwordError: "",
+      submitErrors: ""
+    };
   }
 
-  signup = () => {
-    console.log(this.userInfo.email);
-    const email = this.userInfo.email;
-    const name = this.userInfo.name;
-    const password = this.userInfo.password;
+  change = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
 
-    if (!email.validity.valid) {
-      alert("(" + email.placeholder + ") - " + email.validationMessage);
-      return;
+  validate = () => {
+    let isError = false;
+    const errors = {
+      nameError: "",
+      emailError: "",
+      passwordError: ""
+    };
+
+    if (this.state.name.length == 0) {
+      isError = true;
+      errors.nameError = "Full name is required";
     }
 
-    if (!name.validity.valid) {
-      alert("(" + name.placeholder + ") - " + name.validationMessage);
-      return;
+    if (this.state.email.indexOf("@") === -1) {
+      isError = true;
+      errors.emailError = "Requires valid email";
     }
 
-    if (!password.validity.valid) {
-      alert("(" + password.placeholder + ") - " + password.validationMessage);
-      return;
+    if (this.state.password.length == 0) {
+      isError = true;
+      errors.passwordError = "Password is required";
     }
 
+    this.setState({
+      ...this.state,
+      ...errors
+    });
 
-    fetch(BASE_URL + 'user/add', {
-      method: 'POST',
-      body: JSON.stringify({
-        name: name.value,
-        email: email.value,
-        password: password.value
-      })
-    })
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        if (responseJson.code === 400 && responseJson.message) {
-          alert(responseJson.message);
-          return;
-        }
+    return isError;
+  };
 
-        alert("Success");
-        this.props.history.push('/AddBusiness');
-      })
-      .catch((error) => {
-        console.log(error);
+  onSubmit = e => {
+    e.preventDefault();
+    // this.props.onSubmit(this.state);
+    const err = this.validate();
+    if (!err) {
+      // clear form
+      this.setState({
+        nameError: "",
+        emailError: "",
+        passwordError: "",
+        submitErrors: ""
       });
-  }
+
+      fetch(BASE_URL + 'user/add', {
+        method: 'POST',
+        body: JSON.stringify({
+          name: this.state.name,
+          email: this.state.email,
+          password: this.state.password
+        })
+      })
+        .then((response) => response.json())
+        .then((responseJson) => {
+          console.log(responseJson);
+          if (responseJson.code === 400 && responseJson.message) {
+            this.setState({submitErrors: responseJson.message});
+            return;
+          }
+
+          this.props.history.push('/AddBusiness');
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
 
   render() {
     return (
@@ -70,43 +105,78 @@ class SignUpContainer extends Component {
           <div className="heading" >Get started with a free account</div>
           <div className='login' >Already have an account? <Link to="/Login">Log In</Link></div>
         </div>
-        <div className='inputMainContainer signUpInputCont' >
-          <div className='inputContainer centeredInput' >
-            <input type='email' placeholder='Email' ref={(input) => this.userInfo.email = input} required />
-          </div>
-          <div className='inputContainer centeredInput' >
-            <input type='text' placeholder='Full Name' ref={(nameInput) => this.userInfo.name = nameInput} required />
-          </div>
-          <div className='inputContainer centeredInput' >
-            <input type='password' placeholder='Password' ref={(passwordInput) => this.userInfo.password = passwordInput} required />
-          </div>
-        </div>
+        <form>
+          <MuiThemeProvider>
+            <div>
+              <div className='inputMainContainer signUpInputCont'>
+                <div className='inputContainer centeredInput' style={{color: 'rgb(244, 67, 54)'}}>
+                  {this.state.submitErrors}
+                  </div>
+              </div>
+              <div className='inputMainContainer signUpInputCont' >
+                <div className='inputContainer centeredInput' >
+                  <TextField
+                    name="email"
+                    hintText="Email"
+                    value={this.state.email}
+                    onChange={e => this.change(e)}
+                    errorText={this.state.emailError}
+                    type="email"
+                  />
+                  {/* <input type='email' placeholder='Email' ref={(input) => this.userInfo.email = input} required /> */}
+                </div>
+                <div className='inputContainer centeredInput' >
+                  {/* <input type='text' placeholder='Full Name' ref={(nameInput) => this.userInfo.name = nameInput} required /> */}
+                  <TextField
+                    name="name"
+                    hintText="Full name"
+                    value={this.state.name}
+                    onChange={e => this.change(e)}
+                    errorText={this.state.nameError}
+                  />
+                </div>
+                <div className='inputContainer centeredInput' >
+                  <TextField
+                    name="password"
+                    hintText="Password"
+                    value={this.state.password}
+                    onChange={e => this.change(e)}
+                    errorText={this.state.passwordError}
+                    type="password"
+                  />
+                  {/* <input type='password' placeholder='Password' ref={(passwordInput) => this.userInfo.password = passwordInput} required /> */}
+                </div>
+              </div>
 
-        <div className='footer' >
-          <div className='btnContainer' >
-            <button onClick={this.signup} className="btn orangeBtn">Sign Up</button>
-            {/* <Link to='/AddBusiness' className="btn orangeBtn">Sign Up</Link> */}
-            <div className='orTxtMainCont' >
-              <div className='orLineCont' ><div className='orLine' /></div>
-              <div className='orTxt' >Or</div>
-              <div className='orLineCont' ><div className='orLine' /></div>
+              <div className='footer' >
+                <div className='btnContainer' >
+                  <button onClick={e => this.onSubmit(e)} className="btn orangeBtn">Sign Up</button>
+                  {/* <Link to='/AddBusiness' className="btn orangeBtn">Sign Up</Link> */}
+                  <div className='orTxtMainCont' >
+                    <div className='orLineCont' ><div className='orLine' /></div>
+                    <div className='orTxt' >Or</div>
+                    <div className='orLineCont' ><div className='orLine' /></div>
+                  </div>
+                  <div className='AnotherWayAdviceSL' >Sign up with your Facebook or Google Account</div>
+                </div>
+
+                <div className="btnGroup" >
+                  <button className="btn1"> <span>
+                    <img src={require('../../assets/ficom.png')} style={{ width: 25, height: 25 }} />
+                    {/* <i class="fab fa-facebook-square"></i> */}
+                  </span> Facebook</button>
+                  <button className="btn1" > <span>
+                    <img src={require('../../assets/gicon.png')} style={{ width: 25, height: 25 }} />
+                    {/* <i class="fab fa-google"></i> */}
+                  </span> Google</button>
+                </div>
+
+                <div className="condition" >By clicking sign up, you agree to the Terms and Conditions of HungryHour.</div>
+              </div>
             </div>
-            <div className='AnotherWayAdviceSL' >Sign up with your Facebook or Google Account</div>
-          </div>
+          </MuiThemeProvider>
 
-          <div className="btnGroup" >
-            <button className="btn1"> <span>
-              <img src={require('../../assets/ficom.png')} style={{ width: 25, height: 25 }} />
-              {/* <i class="fab fa-facebook-square"></i> */}
-            </span> Facebook</button>
-            <button className="btn1" > <span>
-              <img src={require('../../assets/gicon.png')} style={{ width: 25, height: 25 }} />
-              {/* <i class="fab fa-google"></i> */}
-            </span> Google</button>
-          </div>
-
-          <div className="condition" >By clicking sign up, you agree to the Terms and Conditions of HungryHour.</div>
-        </div>
+        </form>
       </div>
     );
   }
